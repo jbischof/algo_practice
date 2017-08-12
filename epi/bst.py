700dcd11bc34b1d2a3d5face53f76021d638bc24
Jonathan Bischof <jbischof1@gmail.com>
2017-08-11 17:30:52 -0700

    BST practice

--- epi/bst.py
+++ epi/bst.py
@@ -1,4 +1,5 @@
 import binary_tree as bt
+import collections
 
 class BST(bt.BinaryTree):
   def check_valid(self):
@@ -23,41 +24,128 @@ class BST(bt.BinaryTree):
       return False
     return True
 
+  def search_with_parent(self, value):
+    """ Search for value in BST, finding parent as well.
+    Args:
+      value: Value to find
+    Returns:
+      (Node, parent node) tuple or None if not found
+    Time Complexity: O(H), where H is height of tree
+    Space Complexity: O(1) """
+    if not self.root:
+      return None
+    return self._search_with_parent_helper(self.root, None, value)
+
+  def _search_with_parent_helper(self, node, parent, value):
+    if value == node.value:
+      return (node, parent)
+    if value < node.value:
+      if not node.left:
+        return None 
+      return self._search_with_parent_helper(node.left, node, value)
+    if value > node.value:
+      if not node.right:
+        return None 
+      return self._search_with_parent_helper(node.right, node, value)
+
   def search(self, value):
     """ Search for value in BST.
     Args:
       value: Value to find
     Returns:
-      Boolean
+      Node if found for None if not 
     Time Complexity: O(H), where H is height of tree
     Space Complexity: O(1) """
-    return self.search_helper(self.root, value)
+    ret = self.search_with_parent(value)
+    if ret is None:
+      return None
+    return ret[0]
 
-  def search_helper(self, node, value):
-    if value == node.value:
-      return True
-    if value < node.value:
-      if not node.left:
-        return False
-      return self.search_helper(node.left, value)
-    if value > node.value:
-      if not node.right:
-        return False
-      return self.search_helper(node.right, value)
+  def insert(self, node):
+    """ Insert new value into BST.
+    Time complexity: O(log N) if balanced """
+    if not self.root:
+      self.root = node
+      return
+    current = self.root
+    while True:
+      if node.value > current.value:
+        if current.right:
+          current = current.right
+        else:
+          current.right = node
+          break
+      else:
+        if current.left:
+          current = current.left
+        else:
+          current.left = node
+          break
 
+  def remove_value(self, value):
+    """ Remove first occurrence of value from BST
+    Args:
+      value: Value to be removed from tree
+    Returns:
+      Removed node or None if value not found
+    Time complexity: O(log N) if balanced """
+    # Find node in tree with parent
+    res = self.search_with_parent(value)
+    if not res:
+      return None
+    node, parent = res
+    if node.left and node.right:
+      # If two children, need to replace with next largest node
+      # (smallest node in right subtree)
+      # Find and remove next largest from tree recursively
+      next_largest = self.next_largest(node.value)
+      self.remove_value(next_largest.value)
+      # Put next_largest into parent and child relations of removed node
+      if parent and parent.left is node:
+        parent.left = next_largest
+      elif parent and parent.right is node:
+        parent.right = next_largest
+      next_largest.left = node.left
+      next_largest.right = node.right
+    elif node.left:
+      # If only one child, hook directly to parent node
+      if parent and parent.left is node:
+        parent.left = node.left
+      elif parent and parent.right is node:
+        parent.right = node.left
+    elif node.right:
+      # If only one child, hook directly to parent node
+      if parent and parent.left is node:
+        parent.left = node.right
+      elif parent and parent.right is node:
+        parent.right = node.right
+    else:
+      # Node is childless, delete directly
+      if parent and parent.left is node:
+        parent.left = None
+      elif parent and parent.right is node:
+        parent.right = None
+    # Clear and return node
+    node.left = node.right = None
+    return node
+    
   def next_largest(self, value):
     """ Search for next largest value in BST.
     Args:
       value: baseline value
     Returns:
-      Value or None if 'value' larger than entire tree      
+      Node or None if 'value' larger than entire tree      
     Time Complexity: O(H), where H is height of tree
     Space Complexity: O(1) """
     return self.next_largest_helper(self.root, value, None)
 
   def next_largest_helper(self, node, value, last_largest):
+    """ Search for value with binary search, keeping track of the last largest
+    value seen (which will be the closest in value by definition) in case
+    end search on empty left child (where won't remember last largest you saw.
+    """
     if value < node.value:
-      last_largest = node.value
+      last_largest = node
       if not node.left:
         return last_largest 
       return self.next_largest_helper(node.left, value, last_largest)
@@ -65,3 +153,76 @@ class BST(bt.BinaryTree):
       if not node.right:
         return last_largest
       return self.next_largest_helper(node.right, value, last_largest)
+
+  def k_largest(self, k):
+    """ Return K largest values in BST by reverse inorder traversal
+    Time complexity: O(H + k)
+    Space complexity: O(k) """
+    ret = []
+    self._k_largest_helper(self.root, k, ret)
+    return ret
+  
+  def _k_largest_helper(self, node, k, ret):
+    if node.right:
+      self._k_largest_helper(node.right, k, ret)
+    if len(ret) < k:
+      ret.append(node.value)
+    else:
+      return
+    if node.left:
+      self._k_largest_helper(node.left, k, ret)
+
+  def k_smallest(self, k):
+    """ Return K smallest values in BST by inorder traversal
+    Time complexity: O(H + k)
+    Space complexity: O(k) """
+    ret = []
+    self._k_smallest_helper(self.root, k, ret)
+    return ret
+  
+  def _k_smallest_helper(self, node, k, ret):
+    if node.left:
+      self._k_smallest_helper(node.left, k, ret)
+    if len(ret) < k:
+      ret.append(node.value)
+    else:
+      return
+    if node.right:
+      self._k_smallest_helper(node.right, k, ret)
+
+class Webpage():
+  def __init__(self, id):
+    self.id = id
+
+class WebpageIndex():
+  def __init__(self):
+    self.hashmap = collections.Counter() 
+    self.bst = BST()
+
+  def update(self, page):
+    """ Update page visit counts with Webpage log
+    Args:
+      page: Webpage object
+    Time complexity: O(log N) for BST pop and insert
+    Space complexity: O(1)
+    """
+    old_count = self.hashmap[page.id]
+    self.hashmap[page.id] += 1
+    # Try to remove node and update count
+    node = self.bst.remove_value((old_count, page.id))
+    # Create node if not in tree already
+    if not node:
+      node = bt.Node((0, page.id))
+    # Update count and place in tree
+    node_val = (node.value[0] + 1, node.value[1])
+    self.bst.insert(bt.Node(node_val))
+
+  def update_list(self, page_list):
+    """ Perform update on list of webpages """
+    for page in page_list:
+      self.update(page)
+
+  def top_k_visited(self, k):
+    """ Returns list of ids for k most visited pages """
+    return [val[1] for val in self.bst.k_largest(k)]
+
--- epi/bst_test.py
+++ epi/bst_test.py
@@ -74,10 +74,82 @@ class TestBST(unittest.TestCase):
     self.assertFalse(tree.search(54)) 
     self.assertFalse(tree.search(4))
 
+  def testSearchWithParent(self):
+    tree = self.ExampleTree()
+    self.assertEqual(
+        tree.search_with_parent(17), 
+        (tree.root.left.right.right, tree.root.left.right)) 
+    self.assertEqual(
+        tree.search_with_parent(13), 
+        (tree.root.left.right.right.left, tree.root.left.right.right)) 
+    self.assertEqual(
+        tree.search_with_parent(53), 
+        (tree.root.right.right.right, tree.root.right.right)) 
+    self.assertEqual(
+        tree.search_with_parent(47), 
+        (tree.root.right.right, tree.root.right)) 
+    self.assertEqual(tree.search_with_parent(19), (tree.root, None))
+    self.assertFalse(tree.search_with_parent(20)) 
+    self.assertFalse(tree.search_with_parent(15)) 
+    self.assertFalse(tree.search_with_parent(54)) 
+    self.assertFalse(tree.search_with_parent(4))
+
+  def testInsert(self):
+    tree = self.ExampleTree()
+    node4 = bt.Node(4)
+    tree.insert(node4)
+    self.assertEqual(tree.root.left.left.right.left, node4)
+    tree = self.ExampleTree()
+    node54 = bt.Node(54)
+    tree.insert(node54)
+    self.assertEqual(tree.root.right.right.right.right, node54)
+    # Check inserting into empty tree
+    tree = bst.BST()
+    tree.insert(node4)
+    self.assertEqual(tree.root, node4)
+
+  def testRemove(self):
+    # Remove leaf node
+    tree = self.ExampleTree()
+    self.assertTrue(tree.root.right.left.right.left.right)
+    tree.remove_value(31)
+    self.assertTrue(tree.root.right.left.right.left.right is None)
+    # Remove node with one child
+    tree = self.ExampleTree()
+    self.assertEqual(tree.root.right.left.right.left.value, 29)
+    tree.remove_value(29)
+    self.assertEqual(tree.root.right.left.right.left.value, 31)
+    # Remove node with two children
+    tree = self.ExampleTree()
+    self.assertEqual(tree.root.right.value, 43)
+    tree.remove_value(43)
+    self.assertEqual(tree.root.right.value, 47)
+    self.assertEqual(tree.root.right.right.value, 53)
+    self.assertEqual(tree.root.right.left.value, 23)
+
   def testNextLargest(self):
     tree = self.ExampleTree()
-    self.assertEqual(tree.next_largest(0), 2)
+    self.assertEqual(tree.next_largest(0), tree.root.left.left.left)
     self.assertEqual(tree.next_largest(54), None)
-    self.assertEqual(tree.next_largest(17), 19)
-    self.assertEqual(tree.next_largest(31), 37)
-    self.assertEqual(tree.next_largest(24), 29)
+    self.assertEqual(tree.next_largest(17), tree.root)
+    self.assertEqual(tree.next_largest(31), tree.root.right.left.right)
+    self.assertEqual(tree.next_largest(24), tree.root.right.left.right.left)
+
+  def testKLargest(self):
+    tree = self.ExampleTree()
+    self.assertEqual(tree.k_largest(1), [53])
+    self.assertEqual(tree.k_largest(4), [53, 47, 43, 41])
+
+  def testKSmallest(self):
+    tree = self.ExampleTree()
+    self.assertEqual(tree.k_smallest(1), [2])
+    self.assertEqual(tree.k_smallest(4), [2, 3, 5, 7])
+
+  def testWebpageIndex(self):
+    index = bst.WebpageIndex()
+    index.update_list(
+       [bst.Webpage(i) for i in [1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 4, 4, 5]])
+    self.assertEqual(index.top_k_visited(3), [1, 2, 4]) 
+    # Make '5' most visited page (by one visit)
+    index.update_list([bst.Webpage(i) for i in [5] * 5])
+    self.assertEqual(index.top_k_visited(3), [5, 1, 2]) 
