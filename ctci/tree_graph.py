d7edfa079f0cf91465206a91bff24116b00864d7
Jonathan Bischof <jbischof1@gmail.com>
2018-02-24 21:48:48 -0800

    Tree and graph problems

---
+++ ctci/tree_graph.py
@@ -0,0 +1,223 @@
+"""Tree and graph problems."""
+
+import collections 
+
+
+class Graph(object):
+  """Directed graph based on adjacency list."""
+
+  def __init__(self, adj_dict=None):
+    """Initialize graph.
+    
+    Args:
+      adj_dict: A dict mapping string names to list of string connections. 
+    """
+
+    self.adj_dict = adj_dict or {}
+
+  def isConnected(self, node1, node2):
+    """Returns True is nodes are connected in graph, else False.
+    
+    Args:
+      node1, node2: Names of source and destination nodes.
+    Returns:
+      Boolean.
+    """
+
+    visited = set()
+    queue = collections.deque()
+    queue.append(node1)
+    while queue:
+      curr = queue.popleft()
+      visited.add(curr)
+      for node in self.adj_dict[curr]:
+        if node == node2:
+          return True
+        if not node in visited:
+          queue.append(node)
+    return False
+
+
+class Node(object):
+  def __init__(self, value=None):
+    self.value = value
+    self.left = None
+    self.right = None
+
+
+class Tree(object):
+  def __init__(self, root=None):
+    self.root = root
+
+  @classmethod
+  def from_sorted_list(cls, a):
+    """Construct balanced Tree from sorted list of values."""
+
+    return cls(from_sorted_list_helper(a))
+
+  def inorder(self):
+    """Return inorder traversal of tree."""
+
+    ret = []
+    self.inorder_helper(self.root, ret)
+    return ret
+
+  def inorder_helper(self, node, ret):
+    """Recursive function to compute inorder traversal."""
+
+    if not node:
+      return
+    self.inorder_helper(node.left, ret) 
+    ret.append(node.value)
+    self.inorder_helper(node.right, ret) 
+
+  def level_lists(self):
+    """Write tree values into separate list for each level.
+    
+    Time complexity: O(N)
+    Space complexity: O(H)
+    """
+
+    ret = []
+    self.level_lists_helper(self.root, 0, ret)
+    return ret
+
+  def level_lists_helper(self, node, level, ret):
+    """Recursive function to compute level lists."""
+
+    if not node:
+      return
+    if len(ret) < level + 1:
+      ret.append([])
+    ret[level].append(node.value)
+    self.level_lists_helper(node.left, level + 1, ret) 
+    self.level_lists_helper(node.right, level + 1, ret) 
+
+  def is_balanced(self):
+    """Returns True if height of any two subtrees differs by more than one."""
+
+    return self.is_balanced_helper(self.root)[1]
+
+  def is_balanced_helper(self, node):
+    """Recursive function to check subtree balance.
+
+    Returns:
+      Tuple of (depth, isBalanced) of subtree starting with node
+    """
+
+    # Base case: empty node
+    if not node:
+      return 0, True
+    left_depth, left_bal = self.is_balanced_helper(node.left)
+    right_depth, right_bal = self.is_balanced_helper(node.right)
+    node_bal = abs(left_depth - right_depth) <= 1
+    tree_depth = max(left_depth, right_depth) + 1
+    if not (left_bal and right_bal and node_bal):
+      return tree_depth, False
+    return tree_depth, True
+
+  def is_bst(self):
+    """Check if tree is BST.
+    
+    Time complexity: O(N)
+    Space complexity: O(H)
+    """
+
+    return self.is_bst_helper(self.root, float('-Inf'), float('Inf')) 
+
+  def is_bst_helper(self, node, min_enforce, max_enforce):
+    if not node:
+      return True
+    if not (min_enforce <= node.value < max_enforce):
+      return False
+    return (self.is_bst_helper(node.left, min_enforce, node.value) and
+        self.is_bst_helper(node.right, node.value, max_enforce))
+
+  def inorder_successor(self, node):
+    """Return the inorder successor to node on tree.
+    
+    Assuming parent links.
+
+    Time complexity: O(log N) since may need to ascend entire tree
+    Space complexity: O(1)
+    """
+
+    if node.right:
+      return node.right
+    # Otherwise need to find a left child and return its parent
+    while not node.parent.left is node:
+      # If parent is the root coming from the right, already at greatest node
+      if node.parent is self.root:
+        return None
+      node = node.parent
+    return node.parent
+
+    
+def from_sorted_list_helper(a):
+  """Returns the median value in a sorted list as a Node, or None if empty.
+
+  Populates children nodes recursively.
+  
+  Args:
+    a: A sorted list of values.
+  Returns:
+    A Node object.
+  """
+
+  if not a:
+    return None
+  midpoint = len(a) // 2
+  root = Node(a[midpoint])
+  root.left = from_sorted_list_helper(a[ :midpoint])
+  root.right = from_sorted_list_helper(a[(midpoint + 1): ])
+  return root 
+
+
+def build_order(jobs, depend_pairs):
+  """Determine build order for jobs with dependencies (if feasible).
+
+  This algorithm known as `topological sort`.
+
+  Time complexity: O(|V| + |E|)
+  Space complexity: O(|V| + |E|)
+
+  Args:
+    jobs: List of names of jobs to be performed
+    depend_pairs: Nested list of 2-tuples giving (dependency_job, job) pairs.
+  Returns:
+    Build order list or `None` if no feasible build order.
+  """
+
+  build_order = []
+  inbound_adj_dict = {}
+  outbound_adj_dict = {}
+  # Get dictionaries of inbound and outbound links
+  for job in jobs:
+    inbound_adj_dict[job] = set()
+    outbound_adj_dict[job] = set()
+  for pair in depend_pairs:
+    inbound_adj_dict[pair[1]].add(pair[0])
+    outbound_adj_dict[pair[0]].add(pair[1])
+  # Get list of nodes without dependencies
+  depend_free_jobs = set()
+  remain_jobs = set(jobs)
+  for job, dep_set in inbound_adj_dict.iteritems():
+    if not dep_set:
+      remain_jobs.remove(job)
+      depend_free_jobs.add(job)
+  while depend_free_jobs:
+    depend_job = depend_free_jobs.pop()
+    build_order.append(depend_job)
+    for job in outbound_adj_dict[depend_job]:
+      # Remove depend_job from inbound deps of job
+      inbound_adj_dict[job].remove(depend_job)
+      # If no more dependencies, move to depend_free_jobs
+      if not inbound_adj_dict[job]:
+        remain_jobs.remove(job)
+        depend_free_jobs.add(job)
+  # If still jobs in inbound_adj_dict, not feasible
+  if remain_jobs:
+    return None
+  return build_order
+
+
---
+++ ctci/tree_graph_test.py
@@ -0,0 +1,162 @@
+"""Tests for tree and graph problems."""
+
+import tree_graph as tg
+import unittest
+
+class TreeGraphTest(unittest.TestCase):
+  def ExampleTree(self):
+    """An example tree with parent connections."""
+    a = tg.Node(314)
+    b = tg.Node(6)
+    c = tg.Node(271)
+    d = tg.Node(28)
+    e = tg.Node(0)
+    f = tg.Node(561)
+    g = tg.Node(3)
+    h = tg.Node(17)
+    i = tg.Node(6)
+    j = tg.Node(2)
+    k = tg.Node(1)
+    l = tg.Node(401)
+    m = tg.Node(641)
+    n = tg.Node(257)
+    o = tg.Node(271)
+    p = tg.Node(28)
+    tree = tg.Tree(a)
+    a.left = b
+    b.parent = a
+    b.left = c
+    c.parent = b
+    c.left = d
+    d.parent = c
+    c.right = e
+    e.parent = c
+    b.right = f
+    f.parent = b
+    f.right = g
+    g.parent = f
+    g.left = h
+    h.parent = g
+    a.right = i
+    i.parent = a
+    i.left = j
+    j.parent = i
+    j.right = k
+    k.parent = j
+    k.left = l
+    l.parent = k
+    l.right = m
+    m.parent = l
+    k.right = n
+    n.parent = k
+    i.right = o
+    o.parent = i
+    o.right = p
+    p.parent = o
+    return tree
+
+  def testIsConnected(self):
+    graph = tg.Graph({
+      'a': ['b', 'e'],
+      'b': ['a', 'c', 'd'],
+      'c': ['b', 'd'],
+      'd': ['b', 'c'],
+      'e': ['a'],
+      'g': ['f', 'h'],
+      'f': ['g', 'h'],
+      'h': ['f', 'g']})
+    self.assertTrue(graph.isConnected('a', 'c'))
+    self.assertFalse(graph.isConnected('a', 'g'))
+    self.assertTrue(graph.isConnected('g', 'h'))
+
+  def testFromSortedList(self):
+    a = [0, 3, 4, 5, 11, 15, 17]
+    tree = tg.Tree.from_sorted_list(a)
+    self.assertEqual(tree.root.value, 5)
+    self.assertEqual(tree.root.left.value, 3)
+    self.assertEqual(tree.root.right.value, 15)
+    self.assertEqual(tree.root.left.left.value, 0)
+    self.assertEqual(tree.root.left.right.value, 4)
+    self.assertEqual(tree.root.right.left.value, 11)
+    self.assertEqual(tree.root.right.right.value, 17)
+
+  def testInorder(self):
+    a = [0, 3, 4, 5, 11, 15, 17]
+    tree = tg.Tree.from_sorted_list(a)
+    self.assertEqual(tree.inorder(), a)
+
+  def testLevelLists(self):
+    a = [0, 3, 4, 5, 11, 15, 17]
+    tree = tg.Tree.from_sorted_list(a)
+    self.assertEqual(tree.level_lists(), [[5], [3, 15], [0, 4, 11, 17]])
+
+  def testIsBalanced(self):
+    a = tg.Node('a')
+    b = tg.Node('b')
+    c = tg.Node('c')
+    d = tg.Node('d')
+    e = tg.Node('e')
+    f = tg.Node('f')
+    g = tg.Node('g')
+    tree = tg.Tree(a)
+    a.left = b
+    b.left = c
+    b.right = d
+    d.right = e
+    a.right = f
+    f.right = g
+    # Tree is originally balanced
+    self.assertTrue(tree.is_balanced())
+    # Without node 'g', no longer true
+    f.right = None
+    self.assertFalse(tree.is_balanced())
+
+  def testIsBST(self):
+    a = [0, 3, 4, 5, 11, 15, 17]
+    tree = tg.Tree.from_sorted_list(a)
+    self.assertTrue(tree.is_bst())
+    tree.root.left.right.value = 6
+    self.assertFalse(tree.is_bst())
+      
+  def testInorderSuccessor(self):
+    a = tg.Node('a')
+    b = tg.Node('b')
+    c = tg.Node('c')
+    d = tg.Node('d')
+    e = tg.Node('e')
+    f = tg.Node('f')
+    g = tg.Node('g')
+    tree = tg.Tree(a)
+    a.parent = None
+    a.left = b
+    b.parent = a
+    b.left = c
+    c.parent = b
+    b.right = d
+    d.parent = b
+    d.right = e
+    e.parent = d
+    a.right = f
+    f.parent = a
+    f.right = g
+    g.parent = f
+    # Has right child
+    self.assertEqual(tree.inorder_successor(a), f)
+    # No right child but is left child 
+    self.assertEqual(tree.inorder_successor(c), b)
+    # Biggest node in subtree 
+    self.assertEqual(tree.inorder_successor(e), a)
+    # Biggest node in tree 
+    self.assertEqual(tree.inorder_successor(g), None)
+
+  def testBuildOrder(self):
+    jobs = ['a', 'b', 'c', 'd', 'e', 'f']
+    depend_pairs = [('a', 'd'),
+                    ('f', 'b'),
+                    ('b', 'd'),
+                    ('f', 'a'),
+                    ('d', 'c')]
+    self.assertTrue(tg.build_order(jobs, depend_pairs))
+    # Create a cycle
+    depend_pairs.append(('c', 'a'))
+    self.assertFalse(tg.build_order(jobs, depend_pairs))
