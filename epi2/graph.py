a00e2e1afc99ecfdc5be073a6ed324365d5a65bd
Jonathan Bischof <jbischof@github.com>
2019-02-17 16:02:38 -0800

    Graph problems

---
+++ epi2/graph.py
@@ -0,0 +1,190 @@
+"""Graph problems."""
+import collections
+
+Coordinate = collections.namedtuple('Coordinate', ['x', 'y'])
+
+def _path_from_path_dict(path_dict, start, end):
+  """Recover traversal from path_dict."""
+
+  path = [end]
+  curr = end
+  while curr != start:
+    curr = path_dict[curr]
+    path.append(curr)
+  return path[::-1]
+
+
+def shortest_maze_traversal(start, end, maze):
+  """Find the shortest path between start and end of maze.
+
+  Args:
+    start, end: Coordinate objects indicating start and end points.
+    maze: Square, 2D boolean array where ones are open and zeros blocked squares
+  """
+  
+  queue = collections.deque([start])
+  path_dict = {start : None}
+  curr = None
+
+  while queue:
+    curr = queue.popleft()
+
+    if curr == end:
+      # Path found: return it 
+      return _path_from_path_dict(path_dict, start, end)
+  
+    for pos in [
+        Coordinate(curr.x + 1, curr.y),
+        Coordinate(curr.x, curr.y + 1),
+        Coordinate(curr.x - 1, curr.y),
+        Coordinate(curr.x, curr.y - 1)]:
+      if any(p < 0 or p >= len(maze) for p in pos) or maze[pos.x][pos.y] == 0:
+        continue
+      # Mark position as visited
+      maze[curr.x][curr.y] = 0
+      # Mark origin of point
+      path_dict[pos] = curr
+      queue.append(pos)
+
+  # No path: return None 
+  return None
+
+
+def is_maze_path(start, end, maze):
+  """Determine if there is a path between maze start and end."""
+
+  # Base case: at end point
+  if start == end:
+    return True
+
+  # Mark current point as visited
+  maze[start.x][start.y] = 0
+
+  for pos in [
+      Coordinate(start.x + 1, start.y),
+      Coordinate(start.x, start.y + 1),
+      Coordinate(start.x - 1, start.y),
+      Coordinate(start.x, start.y - 1)]:
+    if any(p < 0 or p >= len(maze) for p in pos) or maze[pos.x][pos.y] == 0:
+      continue
+    if is_maze_path(pos, end, maze):
+      return True
+
+  # Hit bottom of stack without finding end
+  return False
+
+
+class SimpleGraph(object):
+  def __init__(self, adj_list=None):
+    self.adj_list = adj_list or dict()
+
+  def add_vertex(self, vertex, edge_list):
+    self.adj_list[vertex] = edge_list
+
+  def is_connected(self, source, dest):
+    """Return True if path from source to dest node."""
+
+    visited = set()
+    return self.is_connected_helper(source, dest, visited)
+
+  def is_connected_helper(self, source, dest, visited):
+    if source == dest:
+      return True
+
+    visited.add(source)
+
+    for edge in self.adj_list[source]:
+      if edge in visited:
+        continue
+      if self.is_connected_helper(edge, dest, visited):
+        return True
+
+    # Hit bottom of stack without finding dest
+    return False
+
+  def dfs_path(self, source, dest):
+    """Return DFS path from source to destination; None if not connected."""
+    pass
+
+  def is_cyclic(self):
+    """Return True if cycle in graph."""
+
+    # Track colors of nodes. Black nodes are in neither set.
+    # Do not need to track black nodes because are complement of (white, grey) 
+    white = set(self.adj_list.keys()) 
+    grey = set() 
+    while white:
+      if self.is_cyclic_helper(white.pop(), white, grey):
+        return True
+    return False
+
+  def is_cyclic_helper(self, node, white, grey):
+    white.discard(node)
+    grey.add(node)
+    for edge in self.adj_list[node]:
+      if edge in grey:
+        return True
+      if edge in white:
+        if self.is_cyclic_helper(edge, white, grey):
+          return True
+    grey.remove(node)
+    return False
+
+  def topological_sort(self):
+    """Perform topological sort of nodes.
+
+    Note that for topological sort forests are allowed but cycles are not.
+
+    Returns:
+      List of nodes names in sorted order
+
+    Raises:
+      ValueError: Graph is cyclic.
+    """
+    
+    if self.is_cyclic():
+      raise ValueError("Graph is cyclic.")
+
+    white = set(self.adj_list.keys())
+    ret = []
+    while white:
+      self.topological_helper(white.pop(), white, ret)
+    ret.reverse()
+    return ret
+
+  def topological_helper(self, node, white, ret):
+    white.discard(node)
+
+    for edge in self.adj_list[node]:
+      if edge in white:
+        self.topological_helper(edge, white, ret)
+
+    # Add node to ret once black (processed)
+    ret.append(node)
+
+  def shortest_path(self, source, dest):
+    """Find the shortest path between the source and destination nodes.
+    
+    Returns None if no connection.
+    """
+
+    queue = collections.deque([source])
+    path_dict = {source: None}
+    visited = set(source)
+  
+    while queue:
+      curr = queue.popleft()
+
+      if curr == dest:
+        return _path_from_path_dict(path_dict, source, dest)
+
+      for edge in self.adj_list[curr]:
+        if edge not in visited:
+          # Mark position as visited
+          # Need to add visited indicator here so can't add to queue twice
+          visited.add(edge)
+          path_dict[edge] = curr
+          queue.append(edge)
+
+    # If queue empty without hitting dest, not connected
+    return None
---
+++ epi2/graph_test.py
@@ -0,0 +1,93 @@
+import unittest
+import graph
+
+class TestGraph(unittest.TestCase):
+  def defaultMaze(self):
+    return  [[0, 1, 1, 1, 1, 1, 0, 0, 1, 1],
+             [1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+             [0, 1, 0, 1, 1, 0, 0, 1, 0, 0],
+             [1, 1, 1, 0, 0, 0, 1, 1, 0, 1],
+             [1, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+             [1, 0, 0, 1, 1, 0, 1, 0, 0, 1],
+             [1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+             [0, 1, 0, 1, 0, 1, 0, 1, 1, 1],
+             [0, 1, 0, 0, 1, 1, 1, 0, 0, 0],
+             [1, 1, 1, 1, 1, 1, 1, 0, 0, 1]]
+
+  def simpleGraph(self):
+    return graph.SimpleGraph(adj_list={
+        'a': ['b', 'd', 'e'],
+        'b': ['c', 'd'],
+        'c': [],
+        'd': ['a', 'e'],
+        'e': [],
+        })
+
+  def testShortestMazeTraversal(self):
+    self.assertEqual(
+        len(graph.shortest_maze_traversal( 
+            graph.Coordinate(9, 0), graph.Coordinate(0, 9),
+            self.defaultMaze())),
+        19)
+
+  def testIsMazePath(self):
+    self.assertTrue(
+        graph.is_maze_path(graph.Coordinate(9, 0), graph.Coordinate(0, 9),
+            self.defaultMaze()))
+    maze = self.defaultMaze()
+    # Remove access routes to exit
+    maze[1][6], maze[2][7] = 0, 0
+    self.assertFalse(
+        graph.is_maze_path(graph.Coordinate(9, 0), graph.Coordinate(0, 9),
+            maze))
+
+  def testIsConnected(self):
+    g = self.simpleGraph()
+    self.assertTrue(g.is_connected('b', 'a'))
+    # Remove connection between 'd' and 'a'
+    g.adj_list['d'] = ['e']
+    self.assertFalse(g.is_connected('b', 'a'))
+
+  def testShortestPath(self):
+    g = self.simpleGraph()
+    self.assertEqual(g.shortest_path('a', 'e'), ['a', 'e'])
+    self.assertEqual(g.shortest_path('b', 'a'), ['b', 'd', 'a'])
+    # Remove connection between 'd' and 'a'
+    g.adj_list['d'] = ['e']
+    self.assertFalse(g.shortest_path('b', 'a'))
+
+  def testIsCyclic(self):
+    g = graph.SimpleGraph(adj_list={
+        'a': ['b', 'c'],
+        'b': ['c', 'd', 'e'],
+        'c': ['f'],
+        'd': [],
+        'e': [],
+        'f': ['b'],
+        'g': ['f', 'h'],
+        'h': [],
+        })
+    self.assertTrue(g.is_cyclic())
+    # Remove cyclic connection between 'f' and 'b'
+    g.adj_list['f'] = []
+    self.assertFalse(g.is_cyclic())
+    # Add cycle in second tree
+    g.adj_list['h'] = ['g']
+    self.assertTrue(g.is_cyclic())
+
+  def testTopologicalSort(self):
+    g = graph.SimpleGraph(adj_list={
+        'buy_ingredients': ['fry_sausage', 'boil_pasta', 'grate_cheese'],
+        'fry_sausage': ['make_sauce'],
+        'make_sauce': ['assemble_lasagna'],
+        'boil_pasta': ['assemble_lasagna'],
+        'grate_cheese': ['assemble_lasagna'],
+        'assemble_lasagna': ['bake'],
+        'bake': [],
+        'wash_dishes': ['set_table'],
+        'set_table': [],
+        })
+    self.assertEqual(
+        g.topological_sort(),
+        ['wash_dishes', 'set_table', 'buy_ingredients', 'grate_cheese',
+         'boil_pasta', 'fry_sausage', 'make_sauce', 'assemble_lasagna', 'bake'])
