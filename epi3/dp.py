eb0a466b8a798f52085632e15082bb0feedbfea2
Jonathan Bischof <jbischof1@gmail.com>
2022-03-28 21:07:45 -0700

    Longest increasing subsequence

--- epi3/dp.py
+++ epi3/dp.py
@@ -62,3 +62,68 @@ def num_score_combs(n, scores):
                     (memo[i][j - score] if j >= score else 0)
             )
     return memo[n_score - 1][n]
+
+
+def longest_nd_subsequence(a):
+    """
+    Find the length of the longest nondecreasing subsequence in an array.
+
+    Args:
+        a: Array of ints
+    Returns:
+        Int: length of longest nondecreasing subsequence
+
+    Brute force: check all possible subsequences for whether non-decreasing.
+    Time: O(2^N)
+
+    Example:
+         0  1  2  3   4  5   6  7   8  9 
+    a = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9]
+    Ans: [0, 4, 10, 14] and [0, 2, 6, 9], both of length 4
+
+    
+    Recursive approach: can f(a[:j]) be expressed in terms of f(a[:i]), i<j?
+    if f(a[:j-1]) = k, then f(a[:j]) =
+        1 + f(a[:j-1]) if a[j] > a[:j-1]
+
+    Time: O(N^2), Space: O(N)
+
+    i, j, ans, memo
+                0  1  2  3  4  5  6  7  8  9
+    -, -, -,   [1]
+    1, 0, 2,   [1, 2]
+    2, 0, 2
+    2, 1, -,   [1, 2, 2]
+    3, 0, 2
+    3, 1, 3
+    3, 2, 3,   [1, 2, 2, 3]
+    4, 0, 2
+    4, 1, -
+    4, etc     [1, 2, 2, 3, 2]
+    5, 0, 2
+    5, 1, 3
+    5, 2, 3
+    5, 3, -
+    5, 4, 3    [1, 2, 2, 3, 2, 3]
+    6, 0, 2
+    6, 1, -
+    6, 2, 3
+    6, 3, -
+    6, 4, 3
+    6, 5, -    [1, 2, 2, 3, 2, 3]
+    7, 0, 2 -> bigger than everything before so 4
+    7,         [1, 2, 2, 3, 2, 3, 4]
+    etc
+    """
+
+    # Tablular memo. Initialize first entry to one
+    memo = [1] * len(a - 1)
+
+    for i in range(1, len(a)):
+        ans = 1
+        for j in range(i):
+            if a[j] <= a[i]:
+                ans = max(ans, memo[j] + 1)
+        memo[i] = ans
+    return max(memo)
+
--- epi3/dp_test.py
+++ epi3/dp_test.py
@@ -4,3 +4,7 @@ import dp
 class TestDP(unittest.TestCase):
     def test_num_score_combs(self):
         self.assertEqual(dp.num_score_combs(6, [2, 3, 4]), 3)
+
+    def test_longest_nd_subsequence(self):
+        a = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9]
+        self.assertEqual(dp.longest_nd_subsequence(a), 4)
